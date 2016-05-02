a58bd47a56ccbeb122db9c947953051a53920de6
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-05-02 09:30:24 +0200

    Ordered app.js (server script)

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+//inclusions principales---------------------------
 var fs = require('fs');
 
 var express = require('express');
 var app = express();
 
 var server = require('http').Server(app);
+//--------------------------------------------------
 
+//Inclusion de socket.io
 var io = require('socket.io')(server);
 
-//MongoDB
+//Inclusion de MongoDB------------------------------
+
 var MongoClient = require('mongodb').MongoClient;
 
 var assert = require('assert');
@@ -16,18 +20,28 @@ var url = 'mongodb://localhost:27017/CESapp';
 
 var ObjectID = require('mongodb').ObjectID;
 
+//--------------------------------------------------
+
 //App Usings
 
 app.use('/public', express.static( __dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.relocate('/public/views/indeX.html');
+	res.sendFile(__dirname + '/public/views/index.html');
 });
 
+//--------------------------------------------------
+
+
+//Gestion de socket.io------------------------------
 
 io.on('connection', function(socket){
-
+	console.log(socket);
 });
+
+//--------------------------------------------------
+
+//Paramètrage du port du serveur , et de l'adresse MongoDB
 
 var port = process.env.PORT || 3000;
 
@@ -36,3 +50,5 @@ server.listen(port, function(socket) {
 });
 
 var url = 'mongodb://localhost:27017/videodb';
+
+//--------------------------------------------------
