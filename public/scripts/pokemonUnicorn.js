a18d1bbcff8364deeea1036433ee8a2671bdf9d7
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-05-10 23:10:09 +0200

    Page reorganisation and Time Management

--- public/scripts/pokemonUnicorn.js
+++ public/scripts/pokemonUnicorn.js
@@ -5,6 +5,32 @@ var PokeUnicornModule = (function(){
 
 	var calledMonsters = [];
 
+	var playerStats = {
+		maxHP : 20,
+		hP : 20,
+		time : 0,
+		str : 10,
+		def : 12,
+		spe : 9,
+		spA : 10,
+		spd : 11
+	};
+
+	var enemyStats = {
+		maxHP : 20,
+		hP : 20,
+		time : 0,
+		str : 10,
+		def : 10,
+		spe : 10,
+		spA : 11,
+		spd : 11
+	};
+
+	var mainCommands,attackCommands;
+	var enemyHPBar, playerHPBar;
+	var enemySpeedGauge,playerSpeedGauge;
+
 	self.pokemonUnicorn = function() {
 		instance = this;
 		instance._initialize();
@@ -13,9 +39,23 @@ var PokeUnicornModule = (function(){
 	self.pokemonUnicorn.prototype = {
 		_initialize : function() {
 			console.log("initialized");
+			mainCommands = document.getElementsByClassName("mainCommands")[0];
+
+			attackCommands = document.getElementsByClassName("attackCommands")[0];
+
+			enemyHPBar = document.getElementById("enemyActualHP");
+			enemySpeedGauge = document.getElementById("enemyActualSpeed");
+			playerHPBar = document.getElementById("playerActualHP");
+			playerSpeedGauge = document.getElementById("playerActualSpeed");
+
+			requestAnimationFrame(this.update);
+			//setInterval(this.update, 20);
+
 		},
 		selectFight : function() {
 			console.log("Fight !");
+			mainCommands.className = "mainCommands hidden";
+			attackCommands.className = "attackCommands";
 			
 		},
 		selectPokemon : function() {
@@ -26,7 +66,21 @@ var PokeUnicornModule = (function(){
 		},
 		selectSurrender : function() {
 			console.log("I want to give up");
+		},
+		update : function() {
+			console.log("updating");
+
+			if(playerStats.time < 100)
+				playerStats.time += 1*0.2*playerStats.spe*0.5;
+			if(enemyStats.time < 100)
+				enemyStats.time += 1*0.2*enemyStats.spe*0.5;
+
+			playerSpeedGauge.style.width = playerStats.time + "%";
+			enemySpeedGauge.style.width = enemyStats.time + "%";
+
+			requestAnimationFrame(self.pokemonUnicorn.prototype.update);
 		}
+
 	};
 
 	socket.on('attackDone', function() {
